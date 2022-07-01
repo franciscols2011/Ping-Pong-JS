bb26cf2dbb38790c35fbc152d6d726e2c845c0dc
Franciscols2011 <franciscols2011@gmail.com>
2022-07-01 13:38:59 -0300

    Quedo definida la el rectangulo que define la cancha y donde declaramos los elementos que van al prototipo

--- js/app.js
+++ js/app.js
@@ -4,7 +4,8 @@
         this.height = height;
         this.playing = false;
         this.game_over = false;
-        this.bars = []; //
+        this.bars = [];
+        this.ball = null; //
 }
 
 selfBoard.prototype = {
@@ -14,7 +15,23 @@ selfBoard.prototype = {
         return elements;
     }
 }
+(function(){
+    selfBoardView = function(canvas, board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.ctx = canvas.getContext("2d");
+    }
+})();
 
+window.addEventListener("load", main);
+
+function main() {
+    var board = new Board(800, 400);
+    var canvas = document.getElementById('canvas');
+    var board_view = new BoardView(canvas, board);
+}
 
 })();
 function main(){
