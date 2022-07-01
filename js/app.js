c0766a90e9c3c5eb59050bc7e3387dfa49f0da90
Franciscols2011 <franciscols2011@gmail.com>
2022-07-01 16:50:17 -0300

    Agregamos tambien un resetPoint() que se encargarga de devol

--- js/app.js
+++ js/app.js
@@ -228,3 +228,26 @@ function resetGame(){
     document.getElementById("puntajeIzq").innerHTML = 0;
     document.getElementById("puntajeDer").innerHTML = 0;
 }
+
+function resetPoint(){
+    board.playing = false;
+    ball.x = 350;
+    ball.y = 100;
+    ball.direction = 1;
+    ball.bounce_angle = 0;
+    ball.max_bounce_angle = Math.PI / 12;
+    ball.speed_y = 2;
+    ball.speed_x = 10;
+    ball.speed = 10;
+    barIzq.x = 10;
+    barIzq.y = (this.board.height / 2) - 50;
+    barDer.x = this.board.width - 50;
+    barDer.y = (this.board.height / 2) - 50;
+    board_view.clean();
+    board_view.draw();
+}
+
+function controller(){
+    board_view.play();
+    window.requestAnimationFrame(controller);
+}
